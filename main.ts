5fb3c8b38022105ec8d8fb8344f43603c165b0ca
Boeckei <simon.seeboeck@web.de>
2024-12-10 18:05:46 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,11 @@
-let HP = 0
 let zufall = 0
-let Punkte = 0
-input.onGesture(Gesture.ScreenDown, function () {
-    HP = 10
-})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (zufall == 0) {
         basic.showIcon(IconNames.Yes)
         basic.setLedColor(0x00ff00)
-        Punkte += 1
     } else {
         basic.showIcon(IconNames.No)
         basic.setLedColor(0xff0000)
-        HP += -1
     }
     basic.pause(100)
 })
@@ -20,11 +13,9 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     if (zufall == 1) {
         basic.showIcon(IconNames.Yes)
         basic.setLedColor(0x00ff00)
-        Punkte += 1
     } else {
         basic.showIcon(IconNames.No)
         basic.setLedColor(0xff0000)
-        HP += -1
     }
     basic.pause(100)
 })
@@ -72,9 +63,4 @@ basic.forever(function () {
         . . # . .
         . . # . .
         `)
-})
-basic.forever(function () {
-    if (HP == 0) {
-        music.playMelody("C C C C C C C C ", 120)
-    }
 })
